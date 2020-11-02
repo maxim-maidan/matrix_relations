3d8fd37361d6989c7ef7b5b64a965144c1a2738f
maxim-maidan <58174171+maxim-maidan@users.noreply.github.com>
2020-11-02 23:05:25 +0200

    fix: change interface output

--- index.js
+++ index.js
@@ -45,124 +45,150 @@ function setActiveElement(row, col, mat) {
         matrix[mat][row][col] = 1;
     }
 }
-function RelationMatrix(matrix) {
-    this.matrix = matrix;
+class Relation {
+    constructor() {
+        this.setMatrix = matrix => this.matrix = matrix;
+        this.getMatrix = () => this.matrix;
+        this.copyMatrix = () => {
+            let matrix = this.matrix;
+            return matrix;
+        };
+        this.clearMatrix = () => this.matrix = [];
+        this.matrixCrossing = (matrixA, matrixB) => {
+            let res = [];
+            matrixA.forEach((elem, indexY) => {
+                elem.forEach((element, indexX) => {
+                    res[indexY][indexX] = element && matrixB[indexY][indexX];
+                });
+            });
+            return res;
+        };
+        this.matrixUnification = (matrixA, matrixB) => {
+            let res = [];
+            matrixA.forEach((elem, indexY) => {
+                elem.forEach((element, indexX) => {
+                    res[indexY][indexX] = element || matrixB[indexY][indexX];
+                });
+            });
+            return res;
+        };
 
-    this.setMatrix = matrix => this.matrix = matrix;
-    this.getMatrix = () => this.matrix;
-    this.copyMatrix = () => {
-        let matrix = this.matrix;
-        return matrix;
-    }
-    this.clearMatrix = () => this.matrix = [];
-    this.matrixCrossing = (matrixA, matrixB) => {
-        let res = [];
-        matrixA.forEach((elem, indexY) => {
-            elem.forEach((element, indexX) => {
-                res[indexY][indexX] = element && matrixB[indexY][indexX];
-            });
-        });
-        return res;
-    }
-    this.matrixUnification = (matrixA, matrixB) => {
-        let res = [];
-        matrixA.forEach((elem, indexY) => {
-            elem.forEach((element, indexX) => {
-                res[indexY][indexX] = element || matrixB[indexY][indexX];
-            });
-        });
-        return res;
-    }
-
-    this.matrixDifference = (matrixA, matrixB) => {
-        let res = [];
-        for (let i = 0; i < matrixA.length; i++) {
-            res[i] = [];
-            for (let j = 0; j < matrixA[0].length; j++) {
-                res[i][j] = +(matrixA[i][j] > matrixB[i][j]);
-            }
-        }
-        return res;
-    }
-    this.matrixSymmetricDifference = (matrixA, matrixB) => {
-        let res = [];
-        matrixA.forEach((elem, indexY) => {
-            elem.forEach((element, indexX) => {
-                res[indexY][indexX] = element < matrixB[indexY][indexX];
-            });
-        });
-        return res;
-    }
-    this.matrixContain = (matrixA, matrixB) => {
-        return matrixA == matrixB;
-    }
-    this.matrixComposition = (matrixA, matrixB) => {
-        let res = [];
-        for (let i = 0; i < matrixA.length; i++) res[i] = [];
-        for (let k = 0; k < matrixA.length; k++) {
+        this.matrixDifference = (matrixA, matrixB) => {
+            let res = [];
             for (let i = 0; i < matrixA.length; i++) {
-                let t = 0;
-                for (let j = 0; j < matrixA.length; j++) t += matrixA[i][j] * matrixB[j][k];
-                res[i][k] = t;
-            }
-        }
-        return res;
-    }
-    this.matrixAddition = matrix => {
-        let res = [];
-        for (let i = 0; i < matrix.length; i++) {
-            res[i] = [];
-            for (let j = 0; j < matrix[0].length; j++) {
-                res[i][j] = !matrix[i][j];
-            }
-        }
-        return res;
-    }
-    this.matrixTransposition = matrix => {
-        var res = [];
-        for (let i = 0; i < matrix.length; i++) {
-            res[i] = [];
-            for (let j = 0; j < matrix[0].length; j++) {
-                res[i][j] = matrix[j][i];
-            }
-        }
-        return res;
-    }
-    this.matrixDuality = (matrix) => {
-        matrix = this.matrixTransposition(matrix);
-        matrix = this.matrixAddition(matrix);
-        return matrix;
-    }
-
-}
-
-function relationArray(array) {
-    this.array = array;
-    this.setArray = array => this.array = array;
-    this.getArray = () => this.array;
-    this.copyArray = () => {
-        let array = this.array;
-        return array;
-    }
-    this.clearMatrix = () => this.matrix = [];
-
-    this.toMatrix = () => {
-        let matrix = [];
-        this.array.forEach((element) => {
-            matrix[element[0]][element[1]] = 1;
-        });
-        return matrix;
-    }
-    this.toArray = (matrix) => {
-        this.array = [];
-        for (let j = 0; j < matrix.length; j++) {
-            for (let i = 0; i < matrix[0].length; i++) {
-                if (matrix[j][i] == 1) {
-                    this.array.push([i + 1, j + 1]);
+                res[i] = [];
+                for (let j = 0; j < matrixA[0].length; j++) {
+                    res[i][j] = +(matrixA[i][j] > matrixB[i][j]);
                 }
             }
-        }
+            return res;
+        };
+        this.matrixSymmetricDifference = (matrixA, matrixB) => {
+            let res = [];
+            matrixA.forEach((elem, indexY) => {
+                elem.forEach((element, indexX) => {
+                    res[indexY][indexX] = element < matrixB[indexY][indexX];
+                });
+            });
+            return res;
+        };
+        this.matrixContain = (matrixA, matrixB) => {
+            return matrixA == matrixB;
+        };
+        this.matrixComposition = (matrixA, matrixB) => {
+            let res = [];
+            for (let i = 0; i < matrixA.length; i++)
+                res[i] = [];
+            for (let k = 0; k < matrixA.length; k++) {
+                for (let i = 0; i < matrixA.length; i++) {
+                    let t = 0;
+                    for (let j = 0; j < matrixA.length; j++)
+                        t += matrixA[i][j] * matrixB[j][k];
+                    res[i][k] = t;
+                }
+            }
+            return res;
+        };
+        this.matrixAddition = matrix => {
+            let res = [];
+            for (let i = 0; i < matrix.length; i++) {
+                res[i] = [];
+                for (let j = 0; j < matrix[0].length; j++) {
+                    res[i][j] = !matrix[i][j];
+                }
+            }
+            return res;
+        };
+        this.matrixTransposition = matrix => {
+            var res = [];
+            for (let i = 0; i < matrix.length; i++) {
+                res[i] = [];
+                for (let j = 0; j < matrix[0].length; j++) {
+                    res[i][j] = matrix[j][i];
+                }
+            }
+            return res;
+        };
+        this.matrixDuality = (matrix) => {
+            matrix = this.matrixTransposition(matrix);
+            matrix = this.matrixAddition(matrix);
+            return matrix;
+        };
+        this.matrixToHtml = matrix => {
+            let resultString = `<p>`;
+            for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix[0].length; j++) {
+                    resultString = resultString + ` ` + matrix[j][i] + ` `;
+                }
+                resultString = resultString + `<br>`;
+            }
+            resultString = resultString + `</p>`;
+            return resultString;
+        };
+    }
+}
+class RelationMatrix extends Relation {
+    constructor() {
+        super();
+        this.matrix = [];
+    }
+}
 
+class relationArray extends Relation {
+    constructor() {
+        super();
+        this.array = [];
+        this.setArray = array => this.array = array;
+        this.getArray = () => this.array;
+        this.copyArray = () => {
+            let array = this.array;
+            return array;
+        };
+        this.clearMatrix = () => this.matrix = [];
+
+        this.toMatrix = () => {
+            let matrix = [];
+            this.array.forEach((element) => {
+                matrix[element[0]][element[1]] = 1;
+            });
+            return matrix;
+        };
+        this.toArray = (matrix) => {
+            this.array = [];
+            for (let j = 0; j < matrix.length; j++) {
+                for (let i = 0; i < matrix[0].length; i++) {
+                    if (matrix[j][i] == 1) {
+                        this.array.push([i + 1, j + 1]);
+                    }
+                }
+            }
+        };
+        this.arrayToHtml = () => {
+            let str = `<p>`;
+            this.array.forEach(elem => str = str + elem[0] + ` -> ` + elem[1] + `<br>`);
+            str = str + `</p>`;
+            return str;
+        };
     }
 }
 
@@ -185,49 +211,43 @@ function relationArray(array) {
 //     [0, 0, 0, 1, 0]];
 
 function calculateRelations() {
-    console.log(`Матриця P`);
-    console.log(matrix.P);
-    console.log(`Матриця Q`);
-    console.log(matrix.Q);
-    console.log(`Матриця R`);
-    console.log(matrix.R);
-
-    let relation = new RelationMatrix();
+    let m = new RelationMatrix();
 
     let arrayP = new relationArray();
     let arrayQ = new relationArray();
     let arrayR = new relationArray();
     let arrayResult = new relationArray();
     let date = new Date();
+
     let matrixTimeStart = date.getTime();
-
-    let pAndQ = relation.matrixComposition(matrix.P, matrix.Q);
-
-    let d = relation.matrixDuality(matrix.R);
-
-    let k = relation.matrixDifference(pAndQ, d);
+    let pAndQ = m.matrixComposition(matrix.P, matrix.Q);
+    let d = m.matrixDuality(matrix.R);
+    let k = m.matrixDifference(pAndQ, d);
     let matrixTimeEnd = date.getTime();
-    console.log(`Матриця, яка є результатом відношення`);
-    console.log(k);
 
-    console.log(`Час затрачений на виконання обчислень ${matrixTimeStart - matrixTimeEnd} мілісекунд`);
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Матриця, яка є результатом відношення</p>`
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + m.matrixToHtml(k);
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Час затрачений на виконання обчислень ${matrixTimeStart - matrixTimeEnd} мілісекунд</p>`;
+
+    matrixTimeStart = date.getTime(); 
 
     arrayP.toArray(matrix.P);
     arrayQ.toArray(matrix.Q);
     arrayR.toArray(matrix.R);
-    matrixTimeStart = date.getTime();
-    console.log(`Представлення матриці P у вигляді перетинів:`);
-    console.log(arrayP.getArray());
-    console.log(`Представлення матриці Q у вигляді перетинів:`);
-    console.log(arrayQ.getArray());
-    console.log(`Представлення матриці R у вигляді перетинів:`);
-    console.log(arrayR.getArray());
-
     arrayResult.toArray(k);
-    console.log(`Представлення результату у вигляді перетинів:`);
-    console.log(arrayResult.getArray());
+
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Представлення матриці P у вигляді перетинів:</p>`;
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + arrayP.arrayToHtml();
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Представлення матриці Q у вигляді перетинів:</p>`;
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + arrayQ.arrayToHtml();
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Представлення матриці R у вигляді перетинів:</p>`;
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + arrayR.arrayToHtml();
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Представлення результату у вигляді перетинів:</p>`;
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + arrayResult.arrayToHtml();
+
     matrixTimeEnd = date.getTime();
-    console.log(`Час затрачений на виконання обчислень ${matrixTimeStart - matrixTimeEnd} мілісекунд`);
+
+    getElementById('answer').innerHTML = getElementById('answer').innerHTML + `<p>Час затрачений на виконання обчислень ${matrixTimeStart - matrixTimeEnd} мілісекунд</p>`;
 }
 
 
